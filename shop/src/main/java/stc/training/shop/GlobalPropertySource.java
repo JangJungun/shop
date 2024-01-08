3d80dfd260afa12dd2ba9a9972fabb301ac0a008
jjg <wnsrms2ek@naver.com>
2024-01-08 17:50:37 +0900

    test

---
+++ shop/src/main/java/stc/training/shop/GlobalPropertySource.java
@@ -0,0 +1,10 @@
+package stc.training.shop;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySources;
+
+//@Configuration
+//@PropertySources(value = "") 일단보류
+public class GlobalPropertySource {
+
+}
--- shop/src/main/java/stc/training/shop/model/Member.java
+++ shop/src/main/java/stc/training/shop/model/Member.java
@@ -1,5 +1,13 @@
 package stc.training.shop.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Member {
-	
+	private Long id;
+	private String password;
 }
